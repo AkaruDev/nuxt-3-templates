ae67a6ef341cba60e13be561f70c1cf1daa76f79
AdrienAkaru <135602797+AdrienAkaru@users.noreply.github.com>
2023-08-30 15:26:02 +0200

    Progress

--- modules/viewport-observer/playground/pages/index.vue
+++ modules/viewport-observer/playground/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="Page home">
+  <div class="Page Home">
     <h1>Home</h1>
     <nuxt-link to="/page">
       Go to page
     </nuxt-link>
 
-    <div v-observe="{ once: true }" />
+    <div
+      id="ObserveDebugger1"
+      v-observe="{ once: true, onEnter }"
+      class="ObserveDebugger"
+    />
+    <div
+      id="ObserveDebugger2"
+      v-observe="{ once: false }"
+      class="ObserveDebugger"
+    />
   </div>
 </template>
 
@@ -14,8 +23,29 @@
 const { $viewportObserver } = useNuxtApp()
 
 onMounted(() => {
-  console.info($viewportObserver.active.value)
+  setTimeout(() => {
+    $viewportObserver.active.value = true
+  }, 1000);
 })
+
+const onEnter = () => {
+  console.info("onEnter")
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.Page.Home {
+  padding-bottom: 101vh;
+}
+
+.ObserveDebugger {
+  width: 100%;
+  height: 50vh;
+
+  background-color: blue;
+}
+
+.ObserveDebugger.--in-view {
+  background-color: yellow;
+}
+</style>
--- modules/viewport-observer/src/runtime/directives/observe.js
+++ modules/viewport-observer/src/runtime/directives/observe.js
@@ -1,7 +1,61 @@
-export default () => {// nuxtApp
+import { watch } from "vue"
+
+
+export default (nuxtApp) => {
+  const optionsDefault = {
+    activeClass: '--in-view',
+    threshold: 0,
+    offset: 0,
+    once: true,
+    onEnter: null,
+    onLeave: null,
+  }
+
   return {
-    created (el, bindings) {
-      console.info(el, bindings)
-    }
+    /**
+     * @param {HTMLElement} el
+     * @param {*} bindings
+     */
+    mounted (el, bindings) {
+      const options = { ...optionsDefault, ...bindings.value }
+      const { $viewportObserver } = nuxtApp
+
+      // Observer
+      // TODO test with page changes
+      // TODO find a way to clear the observer maybe ?
+      /**
+      * observer @type {IntersectionObserver}
+      */
+      let observer = null
+      const onObserve = ([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          options.onEnter?.()
+          el.classList.add(options.activeClass)
+          if (options.once) {
+            observer.unobserve(el)
+          }
+        } else {
+          options.onLeave?.()
+          el.classList.remove(options.activeClass)
+        }
+      }
+      observer = new IntersectionObserver(onObserve, {
+        root: null,
+        rootMargin: `0px 0px ${-options.offset}px 0px`,
+        threshold: options.threshold
+      })
+      if ($viewportObserver.active.value) observer.observe(el)
+
+      // Watch
+      watch($viewportObserver.active, (newValue) => {
+        if (newValue) {
+          observer.observe(el)
+        } else {
+          observer?.disconnect()
+          el.classList.remove(options.activeClass)
+        }
+      })
+
+    },
   }
 }
