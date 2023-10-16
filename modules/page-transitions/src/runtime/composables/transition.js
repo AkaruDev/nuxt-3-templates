76236eadaf63de734e5620efee94a829c8f9fcee
AdrienAkaru <135602797+AdrienAkaru@users.noreply.github.com>
2023-10-16 14:05:47 +0200

    Adding layout transition

--- modules/page-transitions/src/runtime/composables/transition.js
+++ modules/page-transitions/src/runtime/composables/transition.js
@@ -11,32 +11,35 @@ export const useTransition = (() => {
   const preloader = usePreloader()
   const busTransition = useBusTransition()
 
+  const transition = {
+    css: false,
+    name: 'page',
+    mode: 'out-in',
+    onEnter: (el, done) => {
+      const transitionBus = useBusTransition()
+      transitionBus.onEnter({
+        el, done: () => {
+          transitionBus.onEnterDone(route)
+          done()
+        }, ...route
+      })
+    },
+    onLeave: (el, done) => {
+      const transitionBus = useBusTransition()
+      transitionBus.onLeave({
+        el, done: () => {
+          transitionBus.onLeaveDone(route)
+          done()
+        }, ...route
+      })
+    }
+  }
+
   const instance = {
     busTransition,
     preloader,
-    pageTransition: {
-      css: false,
-      name: 'page',
-      mode: 'out-in',
-      onEnter: (el, done) => {
-        const transitionBus = useBusTransition()
-        transitionBus.onEnter({
-          el, done: () => {
-            transitionBus.onEnterDone(route)
-            done()
-          }, ...route
-        })
-      },
-      onLeave: (el, done) => {
-        const transitionBus = useBusTransition()
-        transitionBus.onLeave({
-          el, done: () => {
-            transitionBus.onLeaveDone(route)
-            done()
-          }, ...route
-        })
-      }
-    },
+    pageTransition: transition,
+    layoutTransition: transition,
     middleware (to, from) {
       route.to = to
       route.from = from
