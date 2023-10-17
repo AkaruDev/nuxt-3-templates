5503a4a594b372dcddcf9f50d757fa76b6faf781
AdrienAkaru <135602797+AdrienAkaru@users.noreply.github.com>
2023-10-17 16:08:02 +0200

    Add preconnect link

--- configs/prismic/nuxt.config.ts
+++ configs/prismic/nuxt.config.ts
@@ -5,34 +5,45 @@ const IS_PREPROD = ENVIRONMENT === 'preprod'
 
 const endpoint = process?.env?.PRISMIC_ENDPOINT
 if (!endpoint) {
-    console.error("Missing prismic endpoint in the environnement variable.")
+  console.error("Missing prismic endpoint in the environnement variable.")
 }
 
 export default defineNuxtConfig(
-    {
-        // ssr: IS_PREPROD ? false : true,
-        modules: [
-            '@nuxtjs/prismic',
-            '@/modules/prismic/src/module',
-            // '@aceforth/nuxt-netlify' // Use if redirects === "netlify"
-        ],
-        prismic: {
-            endpoint,
-            toolbar: IS_PREPROD,
-            preview: IS_PREPROD ? '/preview/' : false,
-            clientConfig: {
-                routes
-            },
-            richTextSerializer: "~/configs/prismic/richTextSerializer"
-        },
-        runtimeConfig: {
-            public: {
-                endpoint,
-                langIso: process?.env?.LANG_ISO || 'fr-fr'
-            }
-        },
-        // "akaru-prismic": {
-        //     redirects: "netlify" // Options : "netlify" or null
-        // }
-    }
+  {
+    // ssr: IS_PREPROD ? false : true,
+    modules: [
+      '@nuxtjs/prismic',
+      '@/modules/prismic/src/module',
+      // '@aceforth/nuxt-netlify' // Use if redirects === "netlify"
+    ],
+    prismic: {
+      endpoint,
+      toolbar: IS_PREPROD,
+      preview: IS_PREPROD ? '/preview/' : false,
+      clientConfig: {
+        routes
+      },
+      richTextSerializer: "~/configs/prismic/richTextSerializer"
+    },
+    runtimeConfig: {
+      public: {
+        endpoint,
+        langIso: process?.env?.LANG_ISO || 'fr-fr'
+      }
+    },
+    app: {
+      head: {
+        link: [
+          {
+            rel: "preconnect",
+            href: "https://prismic.io",
+            crossorigin: "anonymous"
+          }
+        ]
+      }
+    },
+    // "akaru-prismic": {
+    //     redirects: "netlify" // Options : "netlify" or null
+    // }
+  }
 )
