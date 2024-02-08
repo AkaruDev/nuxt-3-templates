afbb1826e2efe15422b31c686d2bc221f1271ae0
AdrienAkaru <135602797+AdrienAkaru@users.noreply.github.com>
2024-02-08 15:05:02 +0100

    Add slugify utils

--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -125,3 +125,18 @@ export const createEllispis = (str, { maxChars = 0, pretty = true, suffix = '...
       .concat(suffix)
   }
 }
+
+export const slugify = (text) => {
+  return text
+    .toString()                   // Cast to string (optional)
+    .normalize('NFKD')            // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+    .replace(/[\u0300-\u036f]/g, '')// deletes all the accents, which happen to be all in the \u03xx UNICODE block
+    .toLowerCase()                  // Convert the string to lowercase letters
+    .trim()                         // Remove whitespace from both sides of a string (optional)
+    .replace(/\s+/g, '-')         // Replace spaces with -
+    .replace(/[^\w\\-]+/g, '')     // Remove all non-word chars
+    .replace(/\\_/g, '-')          // Replace _ with -
+    .replace(/\\-\\-+/g, '-')       // Replace multiple - with single -
+    .replace(/\\-$/g, '');         // Remove trailing -
+}
+
