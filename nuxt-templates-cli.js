99e4a0f5e05efb1ddd3c24d39fb7925f36c09d16
AdrienAkaru <135602797+AdrienAkaru@users.noreply.github.com>
2024-02-20 10:18:04 +0100

    Fix package vuecore to dev

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Add parallax directive/class',
     category: 'Animations'
   },
-  dependencies: ['@vueuse/core'],
-  devDependencies: ['gsap'],
+  dependencies: [],
+  devDependencies: ['@vueuse/core', 'gsap'],
   files: ['modules/parallax/src', 'configs']
 }
