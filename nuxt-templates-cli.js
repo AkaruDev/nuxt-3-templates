a2b8817673528d9a52e4c9bf218ba77accc89fd2
Adrien <dev@akaru.fr>
2024-05-23 16:46:31 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Viewport observer',
-    description: 'Add a directive to add Intersection Observer to elements',
-    category: 'Animations'
-  },
-  dependencies: ["defu"],
-  devDependencies: [],
-  files: ['modules/viewport-observer/src', 'configs']
-}
+export const metas = {
+  title: 'Viewport observer',
+  description: 'Add a directive to add Intersection Observer to elements',
+  category: 'Animations'
+};
+export const dependencies = ["defu"];
+export const devDependencies = [];
+export const files = ['modules/viewport-observer/src', 'configs'];
