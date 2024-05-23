3e93ce8040293ef92b472bf500d6e9509ebf915a
Adrien <dev@akaru.fr>
2024-05-23 16:45:49 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Favicon',
-    description: 'Generate a favicon',
-    category: 'Hosting / Deployment'
-  },
-  dependencies: ['https', 'unzipper'],
-  devDependencies: [],
-  files: ['modules/favicon/src', 'configs', 'public/icons']
-}
+export const metas = {
+  title: 'Favicon',
+  description: 'Generate a favicon',
+  category: 'Hosting / Deployment'
+};
+export const dependencies = ['https', 'unzipper'];
+export const devDependencies = [];
+export const files = ['modules/favicon/src', 'configs', 'public/icons'];
