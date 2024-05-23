59db77b5903f5ccb2687afdc3f11dbfb795311bb
Adrien <dev@akaru.fr>
2024-05-23 16:45:00 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'AppSliders',
-    description: 'Add components',
-    category: 'Components'
-  },
-  dependencies: [],
-  devDependencies: ['@vueuse/core', 'npm:@gsap/shockingly'],
-  files: ['modules/sliders/src', 'configs']
-}
+export const metas = {
+  title: 'AppSliders',
+  description: 'Add components',
+  category: 'Components'
+};
+export const dependencies = [];
+export const devDependencies = ['@vueuse/core', 'npm:@gsap/shockingly'];
+export const files = ['modules/sliders/src', 'configs'];
