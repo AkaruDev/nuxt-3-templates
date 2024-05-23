7f09874066616db53eefa1c29367274a5fbca08a
Adrien <dev@akaru.fr>
2024-05-23 16:41:52 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Netlify',
-    description: 'Add Netlify module (build _redirects/_headers) for Nuxt 3',
-    category: 'Hosting / Deployment'
-  },
-  dependencies: [],
-  devDependencies: [],
-  files: ['modules/netlify/src', 'configs']
-}
+export const metas = {
+  title: 'Netlify',
+  description: 'Add Netlify module (build _redirects/_headers) for Nuxt 3',
+  category: 'Hosting / Deployment'
+};
+export const dependencies = [];
+export const devDependencies = [];
+export const files = ['modules/netlify/src', 'configs'];
