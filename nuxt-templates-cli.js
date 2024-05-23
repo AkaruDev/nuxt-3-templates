ce14717e0d34f43bafa2a9a5fdd62dcb9f9b3df0
Adrien <dev@akaru.fr>
2024-05-23 16:47:03 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Device',
-    description: 'Add a composable to detect breakpoints and mobile/tablet',
-    category: 'Helpers'
-  },
-  dependencies: [],
-  devDependencies: ['@vueuse/core', 'detect-gpu'],
-  files: ['modules/device/src', 'configs']
-}
+export const metas = {
+  title: 'Device',
+  description: 'Add a composable to detect breakpoints and mobile/tablet',
+  category: 'Helpers'
+};
+export const dependencies = [];
+export const devDependencies = ['@vueuse/core', 'detect-gpu'];
+export const files = ['modules/device/src', 'configs'];
