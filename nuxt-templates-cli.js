52ccbe165cfda3a0af2235800a7cbe27120ba40c
Adrien <dev@akaru.fr>
2024-05-23 16:41:24 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Lenis smooth scroll',
-    description: 'Add module for using Lenis smooth scroll',
-    category: 'Scroll'
-  },
-  dependencies: [],
-  devDependencies: ['@vueuse/core', 'gsap', '@studio-freight/lenis'],
-  files: ['modules/lenis-smooth-scroll/src', 'app', 'configs']
-}
+export const metas = {
+  title: 'Lenis smooth scroll',
+  description: 'Add module for using Lenis smooth scroll',
+  category: 'Scroll'
+};
+export const dependencies = [];
+export const devDependencies = ['@vueuse/core', 'gsap', '@studio-freight/lenis'];
+export const files = ['modules/lenis-smooth-scroll/src', 'app', 'configs'];
