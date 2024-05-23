5f1137d22fca4529c114c8591713f8bc93eb52b8
Adrien <dev@akaru.fr>
2024-05-23 16:46:48 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Feature name',
-    description: 'Add feature',
-    category: 'Feature category'
-  },
-  dependencies: [],
-  devDependencies: [],
-  files: ['modules/my-feature/src', 'configs']
-}
+export const metas = {
+  title: 'Feature name',
+  description: 'Add feature',
+  category: 'Feature category'
+};
+export const dependencies = [];
+export const devDependencies = [];
+export const files = ['modules/my-feature/src', 'configs'];
