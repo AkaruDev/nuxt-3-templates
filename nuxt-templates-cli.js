95f15479d6cc64498711c9ad6b52d0ea0285e43a
Adrien <dev@akaru.fr>
2024-05-23 16:44:08 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Page transitions',
-    description: 'Add page loader and pages transitions',
-    category: 'Animations'
-  },
-  dependencies: [],
-  devDependencies: ['@vueuse/core'],
-  files: ['modules/page-transitions/src', 'configs']
-}
+export const metas = {
+  title: 'Page transitions',
+  description: 'Add page loader and pages transitions',
+  category: 'Animations'
+};
+export const dependencies = [];
+export const devDependencies = ['@vueuse/core'];
+export const files = ['modules/page-transitions/src', 'configs'];
