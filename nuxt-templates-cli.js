5f1d8d28062852426753191a2808daf25601529e
Adrien <dev@akaru.fr>
2024-05-23 16:43:04 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Virtual scroll',
-    description: 'Add virtual scroll',
-    category: 'Scroll'
-  },
-  dependencies: [],
-  devDependencies: ['gsap', 'virtual-scroll', '@vueuse/core'],
-  files: ['modules/virtual-scroll/src', 'configs']
-}
+export const metas = {
+  title: 'Virtual scroll',
+  description: 'Add virtual scroll',
+  category: 'Scroll'
+};
+export const dependencies = [];
+export const devDependencies = ['gsap', 'virtual-scroll', '@vueuse/core'];
+export const files = ['modules/virtual-scroll/src', 'configs'];
