02e6caa160db7e03b110f29ef0a610f77fb05bb7
Adrien <dev@akaru.fr>
2024-05-23 16:44:27 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Prismic',
-    description: 'Add Prismic module for Nuxt',
-    category: 'CMS'
-  },
-  dependencies: [],
-  devDependencies: ['@nuxtjs/prismic'],
-  files: ['api', 'modules/prismic/src', 'configs']
-}
+export const metas = {
+  title: 'Prismic',
+  description: 'Add Prismic module for Nuxt',
+  category: 'CMS'
+};
+export const dependencies = [];
+export const devDependencies = ['@nuxtjs/prismic'];
+export const files = ['api', 'modules/prismic/src', 'configs'];
