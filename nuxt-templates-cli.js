01c0451f6ed7aba7d7a54b364f110b792ca74e31
Adrien <dev@akaru.fr>
2024-05-23 16:47:57 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'i18n (NOT READY)',
-    description: 'Add i18n base localisation',
-    category: 'Localization'
-  },
-  dependencies: [],
-  devDependencies: ['@nuxtjs/i18n@next'],
-  files: ['configs', 'i18n.config.js']
-}
+export const metas = {
+  title: 'i18n (NOT READY)',
+  description: 'Add i18n base localisation',
+  category: 'Localization'
+};
+export const dependencies = [];
+export const devDependencies = ['@nuxtjs/i18n@next'];
+export const files = ['configs', 'i18n.config.js'];
