0ce7e1f8a6cbaa25b6177fc0f24206bbf8a9a1b7
Adrien <dev@akaru.fr>
2024-05-23 15:30:29 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'AppImage',
-    description: 'Add image component',
-    category: 'Components'
-  },
-  dependencies: ['@nuxt/image', 'sharp'],
-  devDependencies: ['@vueuse/core', '@vueuse/components'],
-  files: ['.yarnrc', 'modules/app-image/src', 'configs']
-}
+export const metas = {
+  title: 'AppImage',
+  description: 'Add image component',
+  category: 'Components'
+};
+export const dependencies = ['@nuxt/image', 'sharp'];
+export const devDependencies = ['@vueuse/core', '@vueuse/components'];
+export const files = ['.yarnrc', 'modules/app-image/src', 'configs'];
