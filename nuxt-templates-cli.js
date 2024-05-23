e9ec1d45f154f45811ab7cb3c35146f6135f7d49
Adrien <dev@akaru.fr>
2024-05-23 16:41:09 +0200

    Fix export method

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,10 +1,8 @@
-module.exports = {
-  metas: {
-    title: 'Corgi',
-    description: 'Add 3D components utils',
-    category: '3D'
-  },
-  dependencies: ["@types/three", "three-custom-shader-material"],
-  devDependencies: ["vite-plugin-glsl", "gsap", "three", "postprocess"],
-  files: ['modules/corgi/src', 'configs', 'public/draco']
-}
+export const metas = {
+  title: 'Corgi',
+  description: 'Add 3D components utils',
+  category: '3D'
+};
+export const dependencies = ["@types/three", "three-custom-shader-material"];
+export const devDependencies = ["vite-plugin-glsl", "gsap", "three", "postprocess"];
+export const files = ['modules/corgi/src', 'configs', 'public/draco'];
